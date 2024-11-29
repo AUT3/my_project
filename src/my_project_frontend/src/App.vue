f1c186e6bb698a9e5b4c266f6810aeadc19e5c16
AUT3 <paweltomasz2006@gmail.com>
2024-11-29 11:41:09 +0000

    Add Tailwind styles

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -22,17 +22,19 @@ getMsgs();
 </script>
 
 <template>
-  <main>
-    <img src="/logo2.svg" alt="DFINITY logo" />
-    <br />
-    <br />
-    <form action="#" @submit="handleSubmit">
-      <label for="msg">Enter your msg: &nbsp;</label>
-      <input id="msg" alt="msg" type="text" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="displayChat">
-      <div v-for="msg in displayChat">{{ msg }}</div>
-    </section>
+  <main class="flex flex-col items-center justify-center h-dvh">
+    <content class="w-1/3">
+      <img src="/logo2.svg" alt="DFINITY logo" class="w-full" />
+      <br />
+      <br />
+      <form action="#" @submit="handleSubmit">
+        <label for="msg">Enter your msg: &nbsp;</label>
+        <input id="msg" alt="msg" type="text" class="border-black border rounded-md m-5 p-1" />
+        <button type="submit" class="border p-1 rounded-md">Click Me!</button>
+      </form>
+      <section id="displayChat" class="border w-full rounded-md">
+        <div v-for="msg in displayChat" class="p-5 border-b last:border-b-0">{{ msg }}</div>
+      </section>
+    </content>
   </main>
 </template>
